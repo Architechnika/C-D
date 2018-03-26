3c1e3b4e96eb2a46b7a5acb0aa78cba2185ecbf3
Muzantip <mangeti@mail.ru>
2018-03-26 15:48:24 +0400

    Баг с количеством
    исправил баг с отображением количетсво

--- Viewer.js
+++ Viewer.js
@@ -531,11 +531,20 @@ function CodeMapView(backX, backY, backW, backH, fillCol) {
         else if (!isVerticalScreen && codeMapBG) codeMapBG.draw();
 
         if (parent.elems && parent.elems.length > 0) {
+    this.drawCodeMap = function () {
+
+        if (codeMapBG && isVerticalScreen && isSecondScreen) codeMapBG.draw();
+        else if (!isVerticalScreen && codeMapBG) codeMapBG.draw();
+
+        if (parent.elems && parent.elems.length > 0) {
             for(var i=0;i<parent.elems.length;i++)
                 {
                     var el = parent.elems[i];
                     el.draw();
                 }
+            this.menu.draw(); //Отрисовываем дополнительные элементы если нужно
+        }
+    }
             this.menu.draw(); //Отрисовываем дополнительные элементы если нужно
         }
     }
