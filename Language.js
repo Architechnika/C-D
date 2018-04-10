9a4c1cb8ac16a465d900b4c33c5e5b016c2f2da6
Ilya <ilya1591@mail.ru>
2018-04-10 12:23:31 +0300

    Внедрил ачивки и поправил баги

--- Language.js
+++ Language.js
@@ -2,7 +2,10 @@ var lang = {
     ru : {
        'robot_not_know'  : 'Робот не знает что ему делать',
        'inventory_is_empty' : 'Инвентарь робота пуст',
-       'robot_not_look_there' : 'Робот смотрит не в ту сторону',
+       'robot_not_look_there': 'Робот смотрит не в ту сторону',
+       'robot_left_condition_if': 'Не задано условие в операторе условия ЕСЛИ',
+       'robot_left_condition_repeatif': 'Не задано условие в ЦИКЛЕ',
+       'robot_left_counter_countblock': 'Не задано количество итераций в ЦИКЛЕ',
        'crashed_the_wall' : 'Робот врезался в стену',
        'robot_not_find_object' : 'Робот не может найти объект который можно подобрать',
        'game_title' : 'SkillCraft - развивайся играя',
