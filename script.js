88544238440a52aaf8a2dffbe26eb846c2dcd0c6
Muzantip <mangeti@mail.ru>
2018-04-18 13:12:05 +0400

    новый логин

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 // диалог выбора рабочей платформы
 // не вфбрав платформу - нельзя работать
-var login = document.getElementById("user_dialog");
-
+var menu = document.getElementById("header2-0");
+var log = document.getElementById("log");
+log.style.display = 'none';
 // имея сессионной переменной для передачи данных пользователя
 const userSession = "userdata";
 var typeGame = "typeGame"
@@ -19,76 +20,69 @@ var userName = document.getElementById("user_name");
 //          пароль  password
 // глобальный опыт  experiance
 //    текущая игра  game
-var userdata = 
-    {
-        user        : "demo",
-        password    : "",
-        experiance  : "0",
-        game        : "",
-        userID      : ""
-    };
+var userdata = {
+    user: "demo",
+    password: "",
+    experiance: "0",
+    game: "",
+    userID: ""
+};
 //класс для хранения временных данных пользователя
-var tmpUserData = 
-{
+var tmpUserData = {
     userName: "user",
     userId: 'id',
 };
 // определяем событие на закрытие диалога платформы
 // не позволяем закрыть диалог не выбрав плафторму
-login.addEventListener('close', function() 
-    {
-        // очистим содержимое формы ввода данных пользователя
-        document.getElementById("userName").value = ""; 
-        document.getElementById("userPswd").value = "";
+log.addEventListener('close', function () {
+    // очистим содержимое формы ввода данных пользователя
+    document.getElementById("userName").value = "";
+    document.getElementById("userPswd").value = "";
 
-        if(this.returnValue != 'cancel')
-        {
-            //userdata = JSON.parse(this.returnValue);
-            userName.innerHTML = userdata.user;
-        }
-//        userName.innerHTML = userdata.user;
+    if (this.returnValue != 'cancel') {
+        //userdata = JSON.parse(this.returnValue);
+        userName.innerHTML = userdata.user;
     }
-);
+    //        userName.innerHTML = userdata.user;
+});
 
 // отображает диалоговое оккно входа нового пользователя
-function loginShow()
-{
-    login.showModal();
+function loginShow() {
+    menu.style.display = 'none';
+    log.style.display = '';
 }
 
 // определение пользователя в системе и вход его
-function userEnter()
-{
-    userdata.user = document.getElementById("userName").value; 
+function userEnter() {
+    userdata.user = document.getElementById("userName").value;
     userdata.password = document.getElementById("userPswd").value;
-    if(userdata.user.length >0 && userdata.password.length > 0)
-    {
+    if (userdata.user.length > 0 && userdata.password.length > 0) {
         userID = 'id_' + CRC32.str(userdata.user + userdata.password);
         userdata.userID = userID;
         var error = true;
 
-        if(localStorage.getItem(userID))
-        {
-            login.close(localStorage.getItem(userID));
-        }
-        else
-        {
-            //if(confirm('Пользователь:%0\rПароль:%1\rНе зарегестрирован в системе\r\rХотите зарегестрироваться?'.replace('%0', userdata.user).replace('%1', userdata.password)))
-            //{
-                localStorage.setItem(userID, JSON.stringify(userdata));
-                login.close(localStorage.getItem(userID));
-            //}
+        if (localStorage.getItem(userID)) {
+            userCancel();
+        } else {
+            localStorage.setItem(userID, JSON.stringify(userdata));
+            userCancel();
         }
     }
 }
 
 // отмена входа нового пользователя
-function userCancel()
-{
-    login.close('cancel');
+function userCancel() {
+    // login.close('cancel');
+    menu.style.display = '';
+    log.style.display = 'none';
+    tmpUserData.userName = userdata.user;
+    tmpUserData.userId = userID;
+    var tud = JSON.stringify(tmpUserData);
+    sessionStorage.setItem("tmpUserData", tud);
+    localStorage.setItem("tmpUserData", tud);
 }
-function userExit()
-{
+
+function userExit() {
     sessionStorage.removeItem(userSession)
     sessionStorage.removeItem("tmpUserData")
     localStorage.removeItem("tmpUserData");
@@ -99,43 +93,29 @@ function userExit()
 // запускает игру с читыми данными полльзователя
 // если пользователь не новый - стирает старые данные
 // (возможно) выводит запрос на действие
-function newGame()
-{
-    // userID = 'id_' + CRC32.str(userdata.user + userdata.password);
-   // if(userID !== undefined)
-     //   {
-            userdata.experiance = "0";
-            userdata.game = "";
-            sessionStorage.setItem(typeGame, "NewGame");
-            sessionStorage.setItem(userSession, userID);
-            tmpUserData.userName = userdata.user;
-            tmpUserData.userId = userID;
-            var tud = JSON.stringify(tmpUserData);
-            sessionStorage.setItem("tmpUserData",tud);
-            localStorage.setItem("tmpUserData",tud);
-       // }
-   // continueGame();
+function newGame() {
+    userdata.experiance = "0";
+    userdata.game = "";
+    sessionStorage.setItem(typeGame, "NewGame");
+    sessionStorage.setItem(userSession, userID);
 }
 
 // если имеются схранённые днные - загружает их
 // если данных нет - запускается новая игра
-function continueGame()
-{
-   if(userID !== undefined)
-       {
-         sessionStorage.setItem(typeGame, "LoadGame");
+function continueGame() {
+    if (userID !== undefined) {
+        sessionStorage.setItem(typeGame, "LoadGame");
         // передаём ID пользователя в игровой движок
         sessionStorage.setItem(userSession, userID);
         tmpUserData.userName = userdata.user;
         tmpUserData.userId = userID;
         var tud = JSON.stringify(tmpUserData);
-        sessionStorage.setItem("tmpUserData",tud);
-        localStorage.setItem("tmpUserData",tud);
-       }
+        sessionStorage.setItem("tmpUserData", tud);
+        localStorage.setItem("tmpUserData", tud);
+    }
 }
 
 // выводит диалог ностроек программы
-function optionsShow()
-{
+function optionsShow() {
 
 }
