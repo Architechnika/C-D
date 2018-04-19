4bf1e9bf0195f72961f4f0bbfa7c5d0d52352af4
Muzantip <mangeti@mail.ru>
2018-04-19 15:16:31 +0400

    недоделанный ластовый экран

--- LevelLastWindow.js
+++ LevelLastWindow.js
@@ -430,3 +430,28 @@ function setTextTime(secs) {
 }
 
 initLastWindow();
+
+
+function testLastWindow()
+{
+    
+            var base;
+        base = pjs.system.newDOM('div',true);
+        base.innerHTML = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="stylesheet" type="text/css" href="lastWindow/css/style.css" />
+</head>
+
+<body>
+    <p>dfjgfgjdf</p>
+</body>
+
+</html>`
+    
+}
+
+
--- MessageBox.js
+++ MessageBox.js
@@ -8,7 +8,7 @@ function MessageBox()
             <head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-
+                
                 <link rel="stylesheet" type="text/css" href="MessageBox/css/style.css" />
 
 
