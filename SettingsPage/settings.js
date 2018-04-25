9abc65f10ab1e8f5cbdb61826504a773605339dc
Muzantip <mangeti@mail.ru>
2018-04-25 10:46:01 +0400

    222

--- SettingsPage/settings.js
+++ SettingsPage/settings.js
@@ -1,4 +1,5 @@
 var key = "settings";
+var leng = = "ru";
 var cont = document.getElementsByClassName("cont")[0];
 //if(screen.width > screen.height)
 // {
@@ -9,27 +10,37 @@ cont.style.marginLeft = '20%'
 
 //}
 
-if (localStorage.getItem(key)) 
-{
+if (localStorage.getItem(key)) {
     var sett = localStorage.getItem(key);
-     var allSettings = JSON.parse(sett);
+    var allSettings = JSON.parse(sett);
     document.getElementById("audio").checked = allSettings.isAudio;
-    
+
 }
 
 function SettingsAtributs() {
     this.isAudio = document.getElementById("audio").checked;
+    this.language = leng;
 }
 
 function clickReady() {
-    
+
     var allSettings = new SettingsAtributs();
     var tmp = JSON.stringify(allSettings);
     localStorage.setItem(key, tmp);
-    
+
     window.location.href = '../index.html'
 }
 
 function clickCancel() {
     window.location.href = '../index.html'
+}
+
+function lengR() {
+    var rus = document.getElementsByClassName("ru")[0];
+    leng = rus.className;
+}
+
+function lengE() {
+    var eng = document.getElementsByClassName("en")[0];
+    leng = eng.className;
 }
